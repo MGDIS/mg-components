7ff596b8f4f83082db70f803ef860afda20b0cd8
duhem-s <duhem-s@mgdis.fr>
2021-10-01 17:01:52 +0200

    fix(mg-character-left): Issues #5
    BFT-30

--- src/components/mg-character-left/mg-character-left.stories.js
+++ src/components/mg-character-left/mg-character-left.stories.js
@@ -1,13 +1,25 @@
 export default {
   component: 'mg-character-left',
   title: 'Atoms/mg-character-left',
+  parameters: {
+    docs: {
+      source: {
+        code: `<mg-character-left
+  reference="reference-id"
+  characters=""
+  maxlength="400"
+  template="{counter} characters left."
+></mg-character-left>`,
+      },
+    },
+  },
 };
 
-const Template = args => <mg-character-left {...args}>{args.slot}</mg-character-left>;
+const Template = args => <mg-character-left {...args}></mg-character-left>;
 
 export const MgCharacterLeft = Template.bind({});
 MgCharacterLeft.args = {
-  reference: 'test-label',
+  reference: 'reference-id',
   characters: '',
   maxlength: 400,
   template: '{counter} characters left.',
