9309c3b540640a7b3ec6135a6baa58a60ff8bb1f
lelan-j <lelan-j@mgdis.fr>
2021-10-19 08:31:30 +0200

    test: covering mg-input-checkbox

--- src/components/molecules/inputs/mg-input-checkbox/test/mg-input-checkbox.e2e.ts
+++ src/components/molecules/inputs/mg-input-checkbox/test/mg-input-checkbox.e2e.ts
@@ -3,9 +3,9 @@ import { newE2EPage } from '@stencil/core/testing';
 describe('mg-checkbox', () => {
   it('renders', async () => {
     const page = await newE2EPage();
-    await page.setContent('<mg-checkbox></mg-checkbox>');
+    await page.setContent('<mg-input-checkbox></mg-input-checkbox>');
 
-    const element = await page.find('mg-checkbox');
+    const element = await page.find('mg-input-checkbox');
     expect(element).toHaveClass('hydrated');
   });
 });
--- src/components/molecules/inputs/mg-input-checkbox/test/mg-input-checkbox.spec.tsx
+++ src/components/molecules/inputs/mg-input-checkbox/test/mg-input-checkbox.spec.tsx
@@ -1,18 +1,90 @@
+import { h } from '@stencil/core';
 import { newSpecPage } from '@stencil/core/testing';
 import { MgInputCheckbox } from '../mg-input-checkbox';
+import locale from '../../../../../locales';
+
+const getPage = (args) => newSpecPage({
+  components: [MgInputCheckbox],
+  template: () => (<mg-input-checkbox {...args}></mg-input-checkbox>)
+});
 
 describe('mg-input-checkbox', () => {
-  it('renders', async () => {
-    const page = await newSpecPage({
-      components: [MgInputCheckbox],
-      html: `<mg-checkbox></mg-checkbox>`,
-    });
-    expect(page.root).toEqualHtml(`
-      <mg-checkbox>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
-      </mg-checkbox>
-    `);
+  test.each([
+    {label: 'label', identifier: "identifier"},
+    {label: 'label', identifier: "identifier", labelOnTop: true},
+    {label: 'label', identifier: "identifier", readonly: true},
+    {label: 'label', identifier: "identifier", readonly: true, value: true},
+    {label: 'label', identifier: "identifier", readonly: true, value: false},
+    {label: 'label', identifier: "identifier", tooltip: "My Tooltip Message"},
+  ])('Should render with args %s:', async (args) => {
+    const { root } = await getPage(args);
+    expect(root).toMatchSnapshot();
   });
+
+  test.each(["", undefined])('Should not render with invalid label property : %s', async (value) => {
+    try {
+      await getPage({label:value});
+    }
+    catch (err) {
+      expect(err.message).toMatch('prop "label" is required')
+    }
+  });
+
+  test('Should trigger events', async ()=> {
+    const inputValue = false;
+    const args = {label: 'label', identifier: "identifier", helpText: "My help text"};
+    const page = await getPage(args);
+
+    const element = await page.doc.querySelector('mg-input-checkbox');
+    const input = element.shadowRoot.querySelector('input');
+
+    jest.spyOn(page.rootInstance.inputChange, 'emit');
+
+    input.dispatchEvent(new CustomEvent('focus', { bubbles: true }));
+    await page.waitForChanges();
+    expect(page.rootInstance.classes.has('is-focused')).toBeTruthy();
+
+    expect(page.root).toMatchSnapshot(); //Snapshot on focus
+
+    input.value = inputValue.toString();
+    input.dispatchEvent(new CustomEvent('input', { bubbles: true }));
+    await page.waitForChanges();
+    expect(page.rootInstance.inputChange.emit).toHaveBeenCalledWith(inputValue);
+
+    input.dispatchEvent(new CustomEvent('blur', { bubbles: true }));
+    await page.waitForChanges();
+    expect(page.rootInstance.classes.has('is-focused')).toBeFalsy();
+  });
+
+  test.each([
+    {validity: true, valueMissing: false},
+    {validity: false, valueMissing: true},
+    {validity: false, valueMissing: false, value:"Blu"},
+  ])('validity (%s), valueMissing (%s)', async ({validity, valueMissing, value})=> {
+    const args = {label: 'label', identifier: "identifier", value};
+    const page = await getPage(args);
+
+    const element = await page.doc.querySelector('mg-input-checkbox');
+    const input = element.shadowRoot.querySelector('input');
+
+    //mock validity
+    input.checkValidity = jest.fn(()=> validity);
+    Object.defineProperty(input, 'validity', { get: jest.fn(()=> ({
+      valueMissing,
+    }))});
+
+    input.dispatchEvent(new CustomEvent('blur', { bubbles: true }));
+    await page.waitForChanges();
+
+    if(validity) {
+      expect(page.rootInstance.errorMessage).toBeUndefined();
+    }
+    else if (valueMissing){
+      expect(page.rootInstance.errorMessage).toEqual(locale.errors.required);
+    }
+    expect(page.rootInstance.valid).toEqual(validity);
+    expect(page.rootInstance.invalid).toEqual(!validity);
+  });
+
 });
+
