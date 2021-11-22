a605cee59a99bb594e56dc568da9c2b80ff6b7dd
duhem-s <duhem-s@mgdis.fr>
2021-11-22 10:00:42 +0100

    test(mg-button): add tests
    BFT-30

--- src/components/atoms/mg-button/test/mg-button.e2e.tsx
+++ src/components/atoms/mg-button/test/mg-button.e2e.tsx
@@ -14,3 +14,17 @@ describe.each(variants)('mg-button %s', (variant) => {
 
   });
 });
+
+test('Should render a 2 lines button', async ()=>{
+  const page = await createPage(`<mg-button>Button with a<br> two lines text</mg-button>`);
+
+  const screenshot = await page.screenshot();
+  expect(screenshot).toMatchImageSnapshot();
+})
+
+test('Should render a button in a paragraph', async ()=>{
+  const page = await createPage(`<p>This is a <mg-button>button</mg-button> in a paragraph.</p>`);
+
+  const screenshot = await page.screenshot();
+  expect(screenshot).toMatchImageSnapshot();
+})
