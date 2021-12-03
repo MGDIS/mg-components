bc5da740a376cb2b97fc9850a4acf0efa11bb5ad
duhem-s <duhem-s@mgdis.fr>
2021-12-03 15:13:25 +0100

    test(mg-input-text): e2e screenshots
    BFT-297

--- src/components/molecules/inputs/mg-input-text/test/mg-input-text.e2e.ts
+++ src/components/molecules/inputs/mg-input-text/test/mg-input-text.e2e.ts
@@ -1,24 +1,106 @@
-import { newE2EPage } from '@stencil/core/testing';
+import { createPage } from "../../../../../utils/test.utils"
 
 describe('mg-input-text', () => {
 
-  describe.each([false, true])('multiline: %s', (multiline) => {
-
-    test('renders', async () => {
-      const page = await newE2EPage();
-      await page.setContent(`
-      <link rel="stylesheet" href="http://localhost:3333/build/design-system.css" />
-      <mg-input-text label="label" multiline="${multiline}"></mg-input-text>
-    `);
+  describe.each([
+    `<mg-input-text label="label"></mg-input-text>`,
+    `<mg-input-text label="label" label-on-top></mg-input-text>`,
+    `<mg-input-text label="label" label-hide></mg-input-text>`,
+    `<mg-input-text label="label" label-colon placeholder="placeholder" help-text="HelpText Message"></mg-input-text>`
+  ])('without tooltip', (html)=>{
+    test('render', async () => {
+      const page = await createPage(html);
 
       const element = await page.find('mg-input-text');
+      const input = await page.find('mg-input-text >>> input');
+
       expect(element).toHaveClass('hydrated');
 
-      const results = await page.compareScreenshot();
-      expect(results).toMatchScreenshot({ allowableMismatchedPixels: 100 })
-      expect(results).toMatchScreenshot({ allowableMismatchedRatio: 0.1 })
-    });
+      const screenshot = await page.screenshot();
+      expect(screenshot).toMatchImageSnapshot();
 
+      await page.keyboard.down('Tab');
+
+      const screenshotFocus = await page.screenshot();
+      expect(screenshotFocus).toMatchImageSnapshot();
+
+      await input.press("KeyB");
+      await input.press("KeyL");
+      await input.press("KeyU");
+
+      const screenshotType = await page.screenshot();
+      expect(screenshotType).toMatchImageSnapshot();
+    });
+  })
+
+  test('render with tooltip', async () => {
+    const page = await createPage(`<mg-input-text label="label" tooltip="Tooltip message"></mg-input-text>`);
+
+    const element = await page.find('mg-input-text');
+
+    expect(element).toHaveClass('hydrated');
+
+    const screenshot = await page.screenshot();
+    expect(screenshot).toMatchImageSnapshot();
+
+    await page.keyboard.down('Tab');
+    await page.keyboard.down('Tab');
+
+    const screenshotTooltip = await page.screenshot();
+    expect(screenshotTooltip).toMatchImageSnapshot();
+  });
+
+  describe.each([
+    `<mg-input-text label="label" readonly></mg-input-text>`,
+    `<mg-input-text label="label" value="blu"></mg-input-text>`,
+    `<mg-input-text label="label" value="blu" readonly></mg-input-text>`,
+    `<mg-input-text label="label" disabled></mg-input-text>`,
+  ])('Should render with template', (html)=>{
+    test('render', async () => {
+      const page = await createPage(html);
+
+      const element = await page.find('mg-input-text');
+
+      expect(element).toHaveClass('hydrated');
+
+      const screenshot = await page.screenshot();
+      expect(screenshot).toMatchImageSnapshot();
+    });
+  })
+
+  test('Should render error when leaving an empty required input', async () => {
+    const page = await createPage(`<mg-input-text label="label" required></mg-input-text>`);
+
+    const element = await page.find('mg-input-text');
+
+    expect(element).toHaveClass('hydrated');
+
+    await page.keyboard.down('Tab');
+    await page.keyboard.down('Tab');
+
+    const screenshot = await page.screenshot();
+    expect(screenshot).toMatchImageSnapshot();
+  });
+
+  test('Should render error when leaving input with a non matching pattern value', async () => {
+    const page = await createPage(`<mg-input-text label="label" pattern="[a-z]*" pattern-error-message="Only letters lowercase letters."></mg-input-text>`);
+
+    const element = await page.find('mg-input-text');
+    const input = await page.find('mg-input-text >>> input');
+
+    expect(element).toHaveClass('hydrated');
+
+    await page.keyboard.down('Tab');
+
+    await input.press("KeyB");
+    await input.press("KeyL");
+    await input.press("KeyU");
+    await input.press("1");
+
+    await page.keyboard.down('Tab');
+
+    const screenshot = await page.screenshot();
+    expect(screenshot).toMatchImageSnapshot();
   });
 
 });
