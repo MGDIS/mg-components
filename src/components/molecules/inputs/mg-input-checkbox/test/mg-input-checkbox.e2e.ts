d5134ed06723b743128aaa5a02938f88bcdb2911
duhem-s <duhem-s@mgdis.fr>
2022-09-23 15:42:58 +0200

    fix(e2e): add required props for e2e tests
    CORE-1145

--- src/components/molecules/inputs/mg-input-checkbox/test/mg-input-checkbox.e2e.ts
+++ src/components/molecules/inputs/mg-input-checkbox/test/mg-input-checkbox.e2e.ts
@@ -1,11 +1,11 @@
 import { createPage } from '../../../../../utils/test.utils';
 
-describe('mg-checkbox', () => {
+describe('mg-input-checkbox', () => {
   describe.each([
-    `<mg-input-checkbox label="legend"></mg-input-checkbox>`,
-    `<mg-input-checkbox label="legend" label-on-top></mg-input-checkbox>`,
-    `<mg-input-checkbox label="legend" label-hide></mg-input-checkbox>`,
-    `<mg-input-checkbox label="legend" help-text="HelpText Message"></mg-input-checkbox>`,
+    `<mg-input-checkbox identifier="identifier" label="legend"></mg-input-checkbox>`,
+    `<mg-input-checkbox identifier="identifier" label="legend" label-on-top></mg-input-checkbox>`,
+    `<mg-input-checkbox identifier="identifier" label="legend" label-hide></mg-input-checkbox>`,
+    `<mg-input-checkbox identifier="identifier" label="legend" help-text="HelpText Message"></mg-input-checkbox>`,
   ])('without tooltip', html => {
     test('render', async () => {
       const page = await createPage(`${html}
@@ -51,7 +51,7 @@ describe('mg-checkbox', () => {
   });
 
   test.each([true, false])('render with tooltip, case label-on-top %s', async labelOnTop => {
-    const page = await createPage(`<mg-input-checkbox label="legend" tooltip="Tooltip message" label-on-top="${labelOnTop}"></mg-input-checkbox>
+    const page = await createPage(`<mg-input-checkbox identifier="identifier" label="legend" tooltip="Tooltip message" label-on-top="${labelOnTop}"></mg-input-checkbox>
       <script>
       const mgInputCheckbox = document.querySelector('mg-input-checkbox');
       mgInputCheckbox.value = [{title: 'Batman', value: false}, {title: 'Robin', value: false}, {title: 'Joker', value: false}, {title: 'Bane', value: false}];
@@ -82,12 +82,12 @@ describe('mg-checkbox', () => {
   });
 
   describe.each([
-    `<mg-input-checkbox label="legend" readonly></mg-input-checkbox>`,
-    `<mg-input-checkbox label="legend" readonly label-on-top></mg-input-checkbox>`,
-    `<mg-input-checkbox label="legend" disabled></mg-input-checkbox>`,
-    `<mg-input-checkbox label="legend" input-vertical-list help-text="HelpText Message"></mg-input-checkbox>`,
-    `<mg-input-checkbox label="legend" input-vertical-list help-text="HelpText Message" label-on-top></mg-input-checkbox>`,
-    `<mg-input-checkbox label="legend" help-text='<mg-icon icon="user" size="small"></mg-icon> Welcome batman'></mg-input-checkbox>`,
+    `<mg-input-checkbox identifier="identifier" label="legend" readonly></mg-input-checkbox>`,
+    `<mg-input-checkbox identifier="identifier" label="legend" readonly label-on-top></mg-input-checkbox>`,
+    `<mg-input-checkbox identifier="identifier" label="legend" disabled></mg-input-checkbox>`,
+    `<mg-input-checkbox identifier="identifier" label="legend" input-vertical-list help-text="HelpText Message"></mg-input-checkbox>`,
+    `<mg-input-checkbox identifier="identifier" label="legend" input-vertical-list help-text="HelpText Message" label-on-top></mg-input-checkbox>`,
+    `<mg-input-checkbox identifier="identifier" label="legend" help-text='<mg-icon icon="user" size="small"></mg-icon> Welcome batman'></mg-input-checkbox>`,
   ])('Should render with template', html => {
     test('render', async () => {
       const page = await createPage(`${html}
@@ -106,8 +106,8 @@ describe('mg-checkbox', () => {
   });
 
   describe.each([
-    `<mg-input-checkbox label="legend" help-text="HelpText Message" required></mg-input-checkbox>`,
-    `<mg-input-checkbox label="legend" help-text="Message d'aide" lang="fr" required></mg-input-checkbox>`,
+    `<mg-input-checkbox identifier="identifier" label="legend" help-text="HelpText Message" required></mg-input-checkbox>`,
+    `<mg-input-checkbox identifier="identifier" label="legend" help-text="Message d'aide" lang="fr" required></mg-input-checkbox>`,
   ])('%s', html => {
     test('Should render error when leaving an empty required input', async () => {
       const page = await createPage(`${html}
@@ -134,8 +134,8 @@ describe('mg-checkbox', () => {
   });
 
   describe.each([
-    '<mg-input-checkbox label="long label long label long label long label long label long label long label long label long label long label long label" tooltip="tooltip message"></mg-input-checkbox>',
-    '<mg-input-checkbox label="long label long label long label long label long label long label long label long label long label long label long label" tooltip="tooltip message" label-on-top></mg-input-checkbox>',
+    '<mg-input-checkbox identifier="identifier" label="long label long label long label long label long label long label long label long label long label long label long label" tooltip="tooltip message"></mg-input-checkbox>',
+    '<mg-input-checkbox identifier="identifier" label="long label long label long label long label long label long label long label long label long label long label long label" tooltip="tooltip message" label-on-top></mg-input-checkbox>',
   ])('inside a div.mg-form-group', html => {
     test('render', async () => {
       const page = await createPage(`<div class="mg-form-group">${html}</div>
--- src/components/molecules/inputs/mg-input-text/test/mg-input-text.e2e.ts
+++ src/components/molecules/inputs/mg-input-text/test/mg-input-text.e2e.ts
@@ -2,10 +2,10 @@ import { createPage } from '../../../../../utils/test.utils';
 
 describe('mg-input-text', () => {
   describe.each([
-    `<mg-input-text label="label"></mg-input-text>`,
-    `<mg-input-text label="label" label-on-top></mg-input-text>`,
-    `<mg-input-text label="label" label-hide></mg-input-text>`,
-    `<mg-input-text label="label" placeholder="placeholder" help-text="HelpText Message"></mg-input-text>`,
+    `<mg-input-text identifier="identifier" label="label"></mg-input-text>`,
+    `<mg-input-text identifier="identifier" label="label" label-on-top></mg-input-text>`,
+    `<mg-input-text identifier="identifier" label="label" label-hide></mg-input-text>`,
+    `<mg-input-text identifier="identifier" label="label" placeholder="placeholder" help-text="HelpText Message"></mg-input-text>`,
   ])('without tooltip', html => {
     test('render', async () => {
       const page = await createPage(html);
@@ -43,7 +43,7 @@ describe('mg-input-text', () => {
   });
 
   test.each([true, false])('render with tooltip, case label-on-top %s', async labelOnTop => {
-    const page = await createPage(`<mg-input-text label="label" tooltip="Tooltip message" label-on-top="${labelOnTop}"></mg-input-text>`);
+    const page = await createPage(`<mg-input-text identifier="identifier" label="label" tooltip="Tooltip message" label-on-top="${labelOnTop}"></mg-input-text>`);
 
     const element = await page.find('mg-input-text');
 
@@ -67,13 +67,13 @@ describe('mg-input-text', () => {
   });
 
   describe.each([
-    `<mg-input-text label="label" readonly></mg-input-text>`,
-    `<mg-input-text label="label" value="blu"></mg-input-text>`,
-    `<mg-input-text label="label" value="blu" readonly></mg-input-text>`,
-    `<mg-input-text label="label" value="blu" readonly label-on-top></mg-input-text>`,
-    `<mg-input-text label="label" disabled></mg-input-text>`,
-    `<mg-input-text label="label" value="blu" disabled></mg-input-text>`,
-    `<mg-input-text label="label" value="batman" help-text='<mg-icon icon="user" size="small"></mg-icon> Welcome batman'></mg-input-text>`,
+    `<mg-input-text identifier="identifier" label="label" readonly></mg-input-text>`,
+    `<mg-input-text identifier="identifier" label="label" value="blu"></mg-input-text>`,
+    `<mg-input-text identifier="identifier" label="label" value="blu" readonly></mg-input-text>`,
+    `<mg-input-text identifier="identifier" label="label" value="blu" readonly label-on-top></mg-input-text>`,
+    `<mg-input-text identifier="identifier" label="label" disabled></mg-input-text>`,
+    `<mg-input-text identifier="identifier" label="label" value="blu" disabled></mg-input-text>`,
+    `<mg-input-text identifier="identifier" label="label" value="batman" help-text='<mg-icon icon="user" size="small"></mg-icon> Welcome batman'></mg-input-text>`,
   ])('Should render with template', html => {
     test('render', async () => {
       const page = await createPage(html);
@@ -87,7 +87,10 @@ describe('mg-input-text', () => {
     });
   });
 
-  describe.each([`<mg-input-text label="label" required></mg-input-text>`, `<mg-input-text label="label" required lang="fr"></mg-input-text>`])('%s', html => {
+  describe.each([
+    `<mg-input-text identifier="identifier" label="label" required></mg-input-text>`,
+    `<mg-input-text identifier="identifier" label="label" required lang="fr"></mg-input-text>`,
+  ])('%s', html => {
     test('Should render error when leaving an empty required input', async () => {
       const page = await createPage(html);
 
@@ -106,7 +109,9 @@ describe('mg-input-text', () => {
   });
 
   test('Should render error when leaving input with a non matching pattern value', async () => {
-    const page = await createPage(`<mg-input-text label="label" pattern="[a-z]*" pattern-error-message="Vous ne pouvez saisir que des lettres minuscules."></mg-input-text>`);
+    const page = await createPage(
+      `<mg-input-text identifier="identifier" label="label" pattern="[a-z]*" pattern-error-message="Vous ne pouvez saisir que des lettres minuscules."></mg-input-text>`,
+    );
 
     const element = await page.find('mg-input-text');
     const input = await page.find('mg-input-text >>> input');
@@ -129,8 +134,8 @@ describe('mg-input-text', () => {
   });
 
   describe.each([
-    '<mg-input-text label="long label long label long label long label long label long label long label long label long label long label long label" tooltip="tooltip message"></mg-input-text>',
-    '<mg-input-text label="long label long label long label long label long label long label long label long label long label long label long label" tooltip="tooltip message" label-on-top></mg-input-text>',
+    '<mg-input-text identifier="identifier" label="long label long label long label long label long label long label long label long label long label long label long label" tooltip="tooltip message"></mg-input-text>',
+    '<mg-input-text identifier="identifier" label="long label long label long label long label long label long label long label long label long label long label long label" tooltip="tooltip message" label-on-top></mg-input-text>',
   ])('inside a div.mg-form-group', html => {
     test('render', async () => {
       const page = await createPage(`<div class="mg-form-group">${html}</div>`);
@@ -169,7 +174,7 @@ describe('mg-input-text', () => {
       },
     ])('render', async ({ type, slot, icon }) => {
       const page = await createPage(`
-        <mg-input-text label="label" ${icon && 'icon="' + icon + '"'} placeholder="placeholder" type="${type}" readonly="${readonly}" value="bruce">
+        <mg-input-text identifier="identifier" label="label" ${icon && 'icon="' + icon + '"'} placeholder="placeholder" type="${type}" readonly="${readonly}" value="bruce">
           ${slot}
         </mg-input-text>
       `);
@@ -185,7 +190,7 @@ describe('mg-input-text', () => {
 
   describe.each([16, 4, 2])('with custom width: %s', width => {
     test.each([false, true])('with label on top: %s', async labelOnTop => {
-      const page = await createPage(`<mg-input-text label="label" mg-width="${width}" label-on-top="${labelOnTop}"></mg-input-text>`);
+      const page = await createPage(`<mg-input-text identifier="identifier" label="label" mg-width="${width}" label-on-top="${labelOnTop}"></mg-input-text>`);
 
       const element = await page.find('mg-input-text');
 
