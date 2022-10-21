090aea2a149a7a180a4fc4e5eeb73565bd9ae15e
duhem-s <duhem-s@mgdis.fr>
2022-10-21 12:17:40 +0200

    feat(mg-card): new component
    CORE-1326
    
    Revert "feat(mg-card): new component"
    
    This reverts commit 1ca65227fdff073915a7627c34d8f1c02f1657d0.
    
    feat(mg-card): new component
    
    CORE-1326
    
    feat(mg-card): new component
    
    CORE-1326
    
    feat(mg-card): new component
    
    CORE-1326
    
    feat(mg-card): new component
    
    CORE-1326

---
+++ src/components/atoms/mg-card/test/mg-card.e2e.ts
@@ -0,0 +1,19 @@
+import { createPage } from '../../../../utils/test.utils';
+
+describe('mg-card', () => {
+  test('Should render', async () => {
+    const html = [
+      'short text',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    ]
+      .map(slot => `<mg-card>${slot}</mg-card>`)
+      .join('');
+    const page = await createPage(html);
+
+    const element = await page.find('mg-card');
+    expect(element).toHaveClass('hydrated');
+
+    const screenshot = await page.screenshot();
+    expect(screenshot).toMatchImageSnapshot();
+  });
+});
