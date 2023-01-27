e20c34c01b07be67daf96a03967cec85ed9fdb42
jouanno-n <jouanno-n@mgdis.fr>
2023-01-27 16:03:45 +0100

    chore(tsconfig): exclude stories from build
    CORE-2216 #196

--- stencil.config.prod.ts
+++
@@ -1,7 +0,0 @@
-import { Config } from '@stencil/core';
-import { config as defaultConfig } from './stencil.config';
-
-export const config: Config = {
-  ...defaultConfig,
-  tsconfig: 'tsconfig.prod.json',
-};
