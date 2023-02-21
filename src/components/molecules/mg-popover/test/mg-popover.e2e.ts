e511af53066e50e553d194c6df1479107a287c75
duhem-s <duhem-s@mgdis.fr>
2023-02-21 15:44:02 +0100

    fix(mg-popover): improve e2e test stability
    CORE-2297

--- src/components/molecules/mg-popover/test/mg-popover.e2e.ts
+++ src/components/molecules/mg-popover/test/mg-popover.e2e.ts
@@ -44,6 +44,7 @@ describe('mg-popover', () => {
       // display popover on click on slotted element
       mgButton.triggerEvent('click');
       await page.waitForChanges();
+      await page.waitForNetworkIdle();
 
       expect(popover).toHaveAttribute('data-show');
 
@@ -55,6 +56,7 @@ describe('mg-popover', () => {
       // hide popover on click on slotted element
       mgButton.triggerEvent('click');
       await page.waitForChanges();
+      await page.waitForNetworkIdle();
 
       expect(popover).not.toHaveAttribute('data-show');
 
@@ -62,12 +64,14 @@ describe('mg-popover', () => {
       await page.keyboard.down('Tab');
       await page.keyboard.down('Enter');
       await page.waitForChanges();
+      await page.waitForNetworkIdle();
 
       expect(popover).toHaveAttribute('data-show');
 
       // hide popover on keyboad escape key
       await page.keyboard.down('Escape');
       await page.waitForChanges();
+      await page.waitForNetworkIdle();
 
       expect(popover).not.toHaveAttribute('data-show');
     });
